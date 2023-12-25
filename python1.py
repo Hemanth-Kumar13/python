54f2f364e2f928a0d36de87c97ff56ffdb2dbb9e
Hemanth-Kumar13 <111645403+Hemanth-Kumar13@users.noreply.github.com>
2023-12-25 19:02:27 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,18 @@
+def addition(a,b):
+    add = a + b
+    return add
+
+def substraction(a,b):
+    sub = a - b
+    return sub
+
+def multiplication(a,b):
+    multiple = a * b
+    return multiple
+
+x = addition(1,5)
+print(x)
+y = substraction(10,5)
+print(y)
+z = multiplication(4,5)
+print(z)
